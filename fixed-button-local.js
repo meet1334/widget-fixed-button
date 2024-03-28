e1da07898cb44f09c6045ab16083c8e2f26a2674
meet1334 <83622479+meet1334@users.noreply.github.com>
2024-03-28 19:00:19 +0530

    Update fixed-button-local.js

--- fixed-button-local.js
+++ fixed-button-local.js
@@ -13,6 +13,7 @@
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin":'*',
       },
     })
       .then(function (response) {
